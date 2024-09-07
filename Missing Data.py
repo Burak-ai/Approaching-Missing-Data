29da35129442a2b9c4b01a3b3e3f8f3058cd7b0f
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-09-07 21:31:44 +0300

    Update Missing Data.py

--- Missing Data.py
+++ Missing Data.py
@@ -35,3 +35,18 @@ print("MAE from Approach 1 (Drop columns with missing values):")
 print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
 
 
+
+#                 Approach 2 
+my_imputer = SimpleImputer()
+imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+
+imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+
+print("MAE from Approach 2 (Imputation):")
+print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
+
+
